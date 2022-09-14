08c0f5251a60c855dc862cc2262fcb2a0107b5cf
Matt <matt_live@live.co.uk>
2022-09-14 21:30:42 +0100

    add images

--- data/index.js
+++ data/index.js
@@ -1,4 +1,16 @@
-export const navItems = {
-	logo: 'Ori',
+export const navData = {
+	logo: require('../public/img/logo.png'),
 	links: ['Store', 'News', 'Media', 'Studios'],
+};
+
+export const headerData = {
+	title: 'Let the game begin',
+	body: "The little spirit Ori is no stranger to peril, but when a fateful flight puts the owlet Ku in harm's way, it will take more than bravery to bring a family back together, heal a broken land, and discover Ori's true destiny.",
+	images: {
+		imgOne: require('../public/img/ori.png'),
+	},
+};
+
+export const buttonData = {
+	cta: ['Buy now', 'Play'],
 };
